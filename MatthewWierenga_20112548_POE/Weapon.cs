4926c218f9e422ca079a9befff60a7bb358f9344
mattdelta2 <matt.wierenga@outlook.com>
2021-12-03 12:28:02 +0200

    Added Weapon Abstract class

---
+++ MatthewWierenga_20112548_POE/Weapon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatthewWierenga_20112548_POE
+{
+    abstract class Weapon
+    {
+        protected int damage;
+        public int DAMAGE
+        {
+            get { return damage; }
+            set { damage = value; }
+        }
+
+        protected int range;
+        public int RANGE
+        {
+            get { return range; }
+            set { range = value; }
+        }
+
+        protected int durability;
+        public int DURABILITY
+        {
+            get { return durability; }
+            set { durability = value; }
+
+        }
+
+        protected int cost;
+        public int COST
+        {
+            get { return cost; }
+            set { cost = value; }
+        }
+
+        protected string weaponType;
+
+        public string WEAPONTYPE
+        {
+            get { return weaponType; }
+            set { weaponType = value; }
+        }
+    }
+}
