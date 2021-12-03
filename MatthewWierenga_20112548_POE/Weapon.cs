94d965e7bc88ba5bd7dd4d65c786101649ce4978
mattdelta2 <matt.wierenga@outlook.com>
2021-12-03 14:37:42 +0200

    Added constructor and sub classes for weapons

--- MatthewWierenga_20112548_POE/Weapon.cs
+++ MatthewWierenga_20112548_POE/Weapon.cs
@@ -3,26 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Header;
 
 namespace MatthewWierenga_20112548_POE
 {
     abstract class Weapon
     {
         protected int damage;
+        protected int range;
+        protected int durability;
+        protected int cost;
+        protected string weaponType;
+
+        public Weapon(int damage, int range, int durability, int cost, string weaponType)
+        {
+            this.damage = damage;
+            this.range = range;
+            this.durability = durability;
+            this.cost = cost;
+            this.weaponType = weaponType;
+        }
+
         public int DAMAGE
         {
             get { return damage; }
             set { damage = value; }
         }
 
-        protected int range;
+
         public int RANGE
         {
             get { return range; }
             set { range = value; }
         }
 
-        protected int durability;
+
         public int DURABILITY
         {
             get { return durability; }
@@ -30,14 +45,14 @@ namespace MatthewWierenga_20112548_POE
 
         }
 
-        protected int cost;
+
         public int COST
         {
             get { return cost; }
             set { cost = value; }
         }
 
-        protected string weaponType;
+
 
         public string WEAPONTYPE
         {
@@ -46,15 +61,28 @@ namespace MatthewWierenga_20112548_POE
         }
     }
 
-    class MeleeWeapon : Weapon
+
+    internal class MeleeWeapon : Weapon
+        
     {
-        enum MeleeWeapons
+        public MeleeWeapon(int damage, int range, int durability, int cost, string weaponType) : base(damage, range, durability, cost, weaponType)
+        {
+            range = 1;
+           
+
+
+        }
+
+        enum Types
         {
             Dagger,
             Longsword
         }
 
+
         
+
+
 
 
         
@@ -64,8 +92,12 @@ namespace MatthewWierenga_20112548_POE
     }
 
 
-    class RangeWeapon: Weapon
+    class RangeWeapon : Weapon
     {
+        public RangeWeapon(int damage, int range, int durability, int cost, string weaponType) : base(damage, range, durability, cost, weaponType)
+        {
+        }
+
         enum RangeWeapons
         {
             LongBow,
